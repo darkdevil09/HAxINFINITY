78d05785224ba47a70868a505cc070f724855dda
darkdevil09 <125209263+darkdevil09@users.noreply.github.com>
2024-08-03 17:49:00 +0530

    file_ref fixed

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -138,4 +138,5 @@ def unpack_new_file_id(new_file_id):
             decoded.access_hash
         )
     )
-    return file_id
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
