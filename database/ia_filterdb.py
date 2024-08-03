5629ead1591db693b2798faaf73d0e2825ee61eb
darkdevil09 <125209263+darkdevil09@users.noreply.github.com>
2024-08-03 17:59:53 +0530

    file_ref

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -16,6 +16,7 @@ instance = Instance.from_db(db)
 @instance.register
 class Media(Document):
     file_id = fields.StrField(attribute='_id')
+    file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
     caption = fields.StrField(allow_none=True)
